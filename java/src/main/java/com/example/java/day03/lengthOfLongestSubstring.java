ae3812e079c9fd2033db6d5366f9c8fd40403446
zuyunbo <12345678>
2020-04-09 00:29:11 +0800

    lengthOfLongestSubstring

---
+++ java/src/main/java/com/example/java/day03/lengthOfLongestSubstring.java
@@ -0,0 +1,56 @@
+package com.example.java.day03;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class lengthOfLongestSubstring {
+
+
+    /**
+     * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+     *
+     * 示例 1:
+     *
+     * 输入: "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     * 示例 2:
+     *
+     * 输入: "bbbbb"
+     * 输出: 1
+     * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+     * 示例 3:
+     *
+     * 输入: "pwwkew"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++){
+            for (int j = i + 1; j <= n; j++){
+                if (allUnique(s, i, j)){
+                    ans = Math.max(ans, j - i);
+                }
+            }
+
+        }
+        System.out.println(ans);
+    }
+
+    public static boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
+}
