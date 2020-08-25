36aa3ca335a9556136bf95e51212cab469b76d53
zuyunbo <460895202@qq.com>
2020-08-25 14:00:58 +0800

    lengthOfLongestSubstring

--- java/src/main/java/com/example/java/day09/MainCode.java
+++ java/src/main/java/com/example/java/day09/MainCode.java
@@ -3,6 +3,7 @@ package com.example.java.day09;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -25,6 +26,18 @@ public class MainCode {
         List<String> namesFlatStream = nameNested.stream().flatMap(List::stream).collect(Collectors.toList());
         System.out.println(namesFlatStream);
 
+    }
+
+    /**
+     * 列转行,传入的列表进行列转行操作
+     * @param stream 列表数据
+     * @param switchFunc 列转行方法
+     * */
+    public static <T,R> List<R> columnSwitch(Stream<T> stream, Function<T,List<R>> switchFunc){
+        List<R> list = stream.map(switchFunc)
+                .flatMap(List::stream)//降维打击
+                .collect(Collectors.toList());
+        return list;
     }
 
 
